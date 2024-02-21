8c64703ace4bc198ed1f7782c5607552e34f6bfc
Carl Turechek <carl@siteraiser.com>
2024-02-21 13:10:15 -0500

    Update Initialize.php

--- classes/Initialize.php
+++ classes/Initialize.php
@@ -3,12 +3,25 @@ class Initialize extends App {
 	function getProducts(){
 		require_once('system/dbtablesetup.php');
 		$this->loadModel("productModel");
-
+		$this->loadModel('settingsModel');
+		
 		$product_results = $this->productModel->getProductsList();		
 		
 		foreach ($product_results as &$product){
 			$product['iaddress'] = $this->productModel->getIAddresses($product['id']);			
 		}		
-		return ["products"=>$product_results];		
+		
+		
+		//get the apilink
+		$settings = $this->settingsModel->getSettings();
+		$api_url = '';
+		foreach($settings as $setting){
+			//Get wallet address if it hasn't been changed yet.
+			if($setting['name']=='web_api_url'){
+				$api_url = $setting['value'];
+			}
+		}
+
+		return ["api_url"=>$api_url,"products"=>$product_results];		
 	}
 }
