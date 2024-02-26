a76f763e76a3588b2ecd8f35900972a05be06043
Carl Turechek <carl@siteraiser.com>
2024-02-26 14:01:49 -0500

    Update processModel.php

--- models/processModel.php
+++ models/processModel.php
@@ -57,46 +57,27 @@ class processModel extends App {
 	
 	
 	
-	function saveAddress($txid,$id){
+	function saveAddress($address_array){
 		
-		$stmt=$this->pdo->prepare("SELECT id,ship_address FROM responses WHERE crc32 = ? AND (ship_address IS NULL OR ship_address = '')");
-		$stmt->execute([$id]);		
+		$stmt=$this->pdo->prepare("SELECT id,ship_address FROM responses WHERE crc32 = ? ORDER BY id DESC LIMIT 1"); //AND (ship_address IS NULL OR ship_address = '')
+		$stmt->execute([$address_array['id']]);		
 		if($stmt->rowCount()==0){
 			return false;
 		}
-		
-		/*
-		$ship_address = $address_array['n'].PHP_EOL;
-		$ship_address .= $address_array['l1'].PHP_EOL;
-		$ship_address .= $address_array['l2'].PHP_EOL;
-		$ship_address .= $address_array['c1'].PHP_EOL;
-		$ship_address .= $address_array['s'].PHP_EOL;
-		$ship_address .= $address_array['z'].PHP_EOL;
-		$ship_address .= $address_array['c2'].PHP_EOL;		
-		
-		$query='UPDATE responses SET 
-			ship_address=:ship_address
-			WHERE crc32=:crc32';	
-		
-		$stmt=$this->pdo->prepare($query);
-		$stmt->execute(array(
-			':ship_address'=>$ship_address,
-			':crc32'=>$address_array['id']));				
-					
-		if($stmt->rowCount()==0){
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+		if($row['ship_address'] == $address_array['txid']){
 			return false;
-		}		
-		*/
-		
+		}
 		
 		$query='UPDATE responses SET 
 			ship_address=:ship_address
-			WHERE crc32=:crc32';	
+			WHERE crc32=:crc32 AND id=:id';	
 		
 		$stmt=$this->pdo->prepare($query);
 		$stmt->execute(array(
-			':ship_address'=>$txid,
-			':crc32'=>$id));				
+			':ship_address'=>$address_array['txid'],
+			':crc32'=>$address_array['id'],
+			':id'=>$row['id']));				
 					
 		if($stmt->rowCount()==0){
 			return false;
@@ -105,7 +86,10 @@ class processModel extends App {
 		
 		return true;
 	}
-	function addressSubmission($entry){
+	
+	
+	function getAddressArray($entry){
+		
 		$address_string ='';
 		//Find buyer address in payload
 		foreach($entry->payload_rpc as $payload){
@@ -113,31 +97,28 @@ class processModel extends App {
 				$address_string = $payload->value;
 			}				
 		}
-		
-		
-		
 		$given = new DateTime($entry->time);
 		$given->setTimezone(new DateTimeZone("UTC"));	
-		$time_utc = $given->format("Y-m-d H:i:s");
+		$time_utc = $given->format("Y-m-d H:i:s"); 
 		
-		 
-		
-		if($address_string == '' || $time_utc < $this->installed_time_utc){ return false; }
-		
-		
-		/* */
+		if($address_string == '' || $time_utc < $this->installed_time_utc){ return false; }		
+			
 		$address_array = [];
 		$address_parts = explode("?",$address_string);		
 		foreach($address_parts as $part){
 			$temp = explode("$",$part);
 			$address_array[current($temp)]=end($temp);
 		}
+		$address_array['txid'] = $entry->txid;
 		if(!isset($address_array['id'])){
 			return false;
-		}
-		return $this->saveAddress($entry->txid,$address_array['id']);
-		
+		}		
+		return $address_array;
 	}
+	
+
+
+	
 	function txExists($tx){
 
 		$stmt=$this->pdo->prepare("SELECT id FROM incoming WHERE txid = ?");
