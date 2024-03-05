ae2ea1e5c9edffef3a0022d8d43ac413dcb9287c
Carl Turechek <carl@siteraiser.com>
2024-03-04 22:32:13 -0500

    Update Process.php

--- classes/Process.php
+++ classes/Process.php
@@ -103,7 +103,7 @@ class Process extends App {
 
 					$res = $this->webApiModel->newTX($record);	
 					
-				}else if($record['type'] == 'sc_refund_not_enough_tokens'){
+				}else if($record['type'] == 'not_enough_tokens_refund'){
 					//Out of tokens refund confirmed... set i address to inactive
 					$this->loadModel('editProductModel');
 					if($record['for_ia_id'] != ''){
@@ -132,9 +132,12 @@ class Process extends App {
 	//	$export_transfers_result =file_get_contents('testjson.json');
 		
 		$export_transfers_result = json_decode($export_transfers_result);
+		//var_dump($export_transfers_result);
+		
+		
 		if($export_transfers_result === NULL){
 			$errors[] = "Wallet Connection Error.";
-		}else{
+		}else if(isset($export_transfers_result->result->entries)){
 
 			foreach($export_transfers_result->result->entries as $entry){		
 				//See if there is a payload... todo: check if it is a shipping address submission
@@ -215,23 +218,26 @@ class Process extends App {
 				$successful = true;
 			}	
 			if($successful){
-				//Was found and had enough inventory.
-				if($settings['scid'] == '' && $settings['ia_scid'] == ''){
-					if($settings['p_type'] == 'physical'){
-						$tx_list['physical_sales'][] = $tx;
-					}else{
-						$tx_list['digital_sales'][] = $tx;
-					}					
-				}else{
+				//Was found and had enough inventory.$settings['scid'] == '' && $settings['ia_scid'] == ''
+				
+				if($settings['p_type'] == 'physical'){
+					$tx_list['physical_sales'][] = $tx;
+				}else if($settings['p_type'] == 'digital'){
+					$tx_list['digital_sales'][] = $tx;
+				}else if($settings['p_type'] == 'token'){
+					$tx_list['token_sales'][] = $tx;
+				}else if($settings['p_type'] == 'smartcontract'){
 					$tx_list['sc_sales'][] = $tx;
 				}
 			
 			}else if($settings !== false){
-				//No inventory
+				//No inventory$settings['scid'] == '' && $settings['ia_scid'] == ''
 		
-				if($settings['scid'] == '' && $settings['ia_scid'] == ''){
+				if($settings['p_type'] == 'physical'||$settings['p_type'] == 'digital'){
 					$tx_list['refunds'][] = $tx;
-				}else{
+				}else if($settings['p_type'] == 'token'){
+					$tx_list['token_refunds'][] = $tx;
+				}else if($settings['p_type'] == 'smartcontract'){
 					$tx_list['sc_refunds'][] = $tx;
 				}
 				
@@ -282,7 +288,22 @@ class Process extends App {
 	}
 	
 	
-	//Create sc_sales orders.
+	//Create token_sales orders.
+	if(isset($tx_list['token_sales'])){		
+		foreach($tx_list['token_sales'] as $tx){
+			$this->processModel->insertOrder([$tx],'token_sale');
+		}
+	}
+	
+	//Create sc refund orders.
+	if(isset($tx_list['token_refunds'])){		
+		foreach($tx_list['token_refunds'] as $tx){
+			$this->processModel->insertOrder([$tx],'token_refund');
+		}
+	}
+
+
+	//Create token_sales orders.
 	if(isset($tx_list['sc_sales'])){		
 		foreach($tx_list['sc_sales'] as $tx){
 			$this->processModel->insertOrder([$tx],'sc_sale');
@@ -295,9 +316,6 @@ class Process extends App {
 			$this->processModel->insertOrder([$tx],'sc_refund');
 		}
 	}
-
-
-
 
 
 	
@@ -446,17 +464,17 @@ class Process extends App {
 
 
 
-		/*******************************/
-		/***  Do smart contracts as  ***/
-		/***  separate transfers     ***/
-		/*******************************/
+		/***************************/
+		/***  Do token sales as  ***/
+		/***  separate transfers ***/
+		/***************************/
 			
-		//transfer_list	
 
-		if(empty($transfer_list)){//Done with regular response transfer
+		$sent_one = false;//Set here to ensure sc sales know not run to if a token has been sent this time. 
+		if(empty($transfer_list)){//Done with regular response transfer (need to do one at a time to check for insufficient funds failures for token tranfers)
 		
-			$pending_sc_orders = $this->processModel->getOrdersByStatusAndType('pending','sc_sale');	
-			$pending_sc_refunds = $this->processModel->getOrdersByStatusAndType('pending','sc_refund');	
+			$pending_sc_orders = $this->processModel->getOrdersByStatusAndType('pending','token_sale');	
+			$pending_sc_refunds = $this->processModel->getOrdersByStatusAndType('pending','token_refund');	
 			$pending_orders = array_merge($pending_sc_orders,$pending_sc_refunds);
 		
 			$sent_one = false;			
@@ -468,14 +486,13 @@ class Process extends App {
 
 				$successful=false;
 				if($settings !== false){	
-					//Make sure it is a token sale...
 					$successful = $tx['successful'];								
 				}
 			
 				if($successful==1){
 					//Is a smart contract token transfer...				
 					//Send Response to buyer
-					$xfer = $this->createSCTransfer($tx,$settings);	
+					$xfer = $this->createTokenTransfer($tx,$settings);	
 					$tx = $xfer->tx;
 					$sc_transfer = $xfer->xfer;
 
@@ -483,17 +500,17 @@ class Process extends App {
 
 					//No mathcing products with inv. / I. Addresses found
 					//Send Refund to buyer
-					$xfer = $this->createRefundSCTransfer($tx,$settings);	
+					$xfer = $this->createRefundTokenTransfer($tx,$settings);	
 					$tx = $xfer->tx;
 					$sc_transfer = $xfer->xfer;		
 					
 				}else if($successful==2){
 
-					$xfer = $this->createRefundSCTransfer($tx,$settings);	
+					$xfer = $this->createRefundTokenTransfer($tx,$settings);	
 					$tx = $xfer->tx;
 					$sc_transfer = $xfer->xfer;		
 				
-					$tx['type'] = "sc_refund_not_enough_tokens";
+					$tx['type'] = "not_enough_tokens_refund";
 					
 				} 
 				
@@ -537,12 +554,163 @@ class Process extends App {
 				}else{
 					if(isset($payload_result->error)){
 						if(strstr($payload_result->error->message, "Insufficent funds")){
-							$errors[] = "SC Transfer Error. ".$payload_result->error->message;					
+							$errors[] = "Token Transfer Error. ".$payload_result->error->message;					
 							// Save incoming as successful = 2 to signal it was a not enough tokens error...Reprocess...
 							$this->processModel->markIncSuccessfulTwo($tx['id']);		
 						}else{
-							$errors[] = "SC Transfer Error. ".$payload_result->error->message;
+							$errors[] = "Token Transfer Error. ".$payload_result->error->message;
 						}
+					}else{						
+						$errors[] = "Unkown Transfer Error";
+					}
+				}
+				
+				if($responseTXID !== ''){
+					$result = $this->processModel->markOrderAsProcessed($tx['order_id']);
+						
+					$given = new DateTime();
+					$given->setTimezone(new DateTimeZone("UTC"));	
+					$time_utc = $given->format("Y-m-d H:i:s");
+					
+
+					if($result !== false){
+						$response = (object)[
+						"order_id"=>$tx['order_id'],
+						"txid"=>$responseTXID,
+						"type"=>$tx['type'],
+						"buyer_address"=>$tx['buyer_address'],
+						"out_amount"=>$tx['respond_amount'],
+						"port"=>$tx['port'],
+						"out_message"=>$tx['out_message'],
+						"out_message_uuid"=>$tx['out_message_uuid'],
+						"uuid"=>$tx['uuid'],
+						"api_url"=>$tx['api_url'],
+						"out_scid"=>$tx['out_scid'],
+						"crc32"=>$tx['crc32'],
+						"time_utc"=>$time_utc,
+						"t_block_height"=>$t_block_height
+						];
+						
+						
+					$this->processModel->saveResponse($response);
+						
+					$messages[] = "{$tx['type']} response initiated for: {$tx['respond_amount']}". ($tx['type'] == 'token_sale' ? ' for "'.$tx['ia_comment'].'"' : '') . ".";
+				
+					}
+				
+				}
+				
+			}
+
+			unset($tx);
+
+		}
+
+
+
+
+		/*********************************/
+		/***  Do SC ownership sales as ***/
+		/***  separate transfers       ***/
+		/*********************************/
+			
+
+
+		if(empty($transfer_list) && !$sent_one){//Done with regular response transfer (need to do one at a time to check for insufficient funds failures for token tranfers)
+		
+			$pending_sc_orders = $this->processModel->getOrdersByStatusAndType('pending','sc_sale');	
+			$pending_sc_refunds = $this->processModel->getOrdersByStatusAndType('pending','sc_refund');	
+			$pending_orders = array_merge($pending_sc_orders,$pending_sc_refunds);
+		
+			$sent_one = false;			
+			foreach($pending_orders as &$tx){
+				if($sent_one){
+					break;
+				}
+				$settings = $this->processModel->getIAsettings($tx);	
+
+				$successful=false;
+				if($settings !== false){	
+					$successful = $tx['successful'];								
+				}
+			
+				if($successful==1){
+					//Is a smart contract ownership transfer...				
+					//Send Response to buyer
+					$xfer = $this->createSCOTransfer($tx,$settings);	
+					$tx = $xfer->tx;
+					$sc_transfer = $xfer->xfer;
+
+				}else if($successful==0){
+
+					//No mathcing products with inv. / I. Addresses found
+					//Send Refund to buyer
+					$xfer = $this->createRefundSCOTransfer($tx,$settings);	
+					$tx = $xfer->tx;
+					$sc_transfer = $xfer->xfer;		
+					
+				}else if($successful==2){
+
+					$xfer = $this->createRefundSCOTransfer($tx,$settings);	
+					$tx = $xfer->tx;
+					$sc_transfer = $xfer->xfer;		
+				
+					$tx['type'] = "sc_ownership_transfer_refund";
+					
+				} 
+				
+			
+				$responseTXID='';
+				$t_block_height='';
+					
+				//Make sure wallet is working
+				$result_str = $this->deroApiModel->getHeight();
+				$heightRes = json_decode($result_str);
+				if(!isset($heightRes->errors) && isset($heightRes->result)){	
+					$t_block_height = $heightRes->result->height;	
+				}			
+				
+				$payload_result =null;
+				if(is_int($t_block_height)){
+					//try the transfer
+					if($tx['type'] != "sc_ownership_transfer_refund"){
+						//run ownership transfer
+						$payload_result = $this->deroApiModel->transferOwnership($sc_transfer);
+					}else{
+						//dispatch a refund
+						$payload_result = $this->deroApiModel->transfer([$sc_transfer]);
+					}
+					$payload_result = json_decode($payload_result);
+					
+					//Get the actual blockheight or just increment by 1 if it fails since we need to have a height to check for confimation
+					$tbh = '';
+					$result_str = $this->deroApiModel->getHeight();
+					$heightRes = json_decode($result_str);
+					if(!isset($heightRes->errors) && isset($heightRes->result)){	
+						$tbh = $heightRes->result->height;							
+					}					
+					if(is_int($tbh)){
+						$t_block_height = $heightRes->result->height;	
+					}else{
+						++$t_block_height;
+					}		
+				}
+			
+				
+				$sent_one = true;
+				if($payload_result != null && isset($payload_result->result)){
+					
+					$responseTXID = $payload_result->result->txid;
+					
+				}else{
+					if(isset($payload_result->error)){
+						if(1){
+							$errors[] = "SC Ownership Transfer Error. ".$payload_result->error->message;					
+							// Save incoming as successful = 2 to signal it was a not enough tokens error...Reprocess...
+							$this->processModel->markIncSuccessfulTwo($tx['id']);		
+						}/*else{
+							$errors[] = "Token Transfer Error. ".$payload_result->error->message;
+						}*/
 					}else{						
 						$errors[] = "Unkown Transfer Error";
 					}
@@ -589,9 +757,6 @@ class Process extends App {
 
 		}
 
-
-
-
 		if($product_changes){
 			$product_results = $this->productModel->getProductsList();
 			foreach ($product_results as &$product){
@@ -604,6 +769,8 @@ class Process extends App {
 	}
 	
 	
+	
+	//Regular response (message w/ Dero)	
 	public function createTransfer(&$tx,$settings){
 
 		//Send Response to buyer
@@ -622,7 +789,7 @@ class Process extends App {
 		$transfer['out_message'] = $settings['out_message'];				
 			
 		//Check for a pending response for this incoming tx
-		$pending_response = $this->processModel->checkForResponseById($tx['order_id']);
+		$pending_response = $this->processModel->checkForPendingResponseByOrderId($tx['order_id']);
 		if($pending_response !==false){
 			//Found a previous repsonse, use that instead of a new one (in case of double response we want the same confirmation number for address submission)
 			$transfer['out_message'] = $pending_response['out_message'];
@@ -644,7 +811,7 @@ class Process extends App {
 			
 		return (object)["tx"=>$tx,"xfer"=>$transfer_object];
 	}
-	
+	//Regular Refund (w/ Dero)
 	public function createRefundTransfer(&$tx,$settings){	
 		$transfer['respond_amount'] = $tx['amount'];
 		$transfer['address'] = $tx['buyer_address'];	
@@ -664,7 +831,8 @@ class Process extends App {
 		return (object)["tx"=>$tx,"xfer"=>$transfer_object];
 	}
 	
-	public function createSCTransfer(&$tx,$settings){		
+	//Token transfer with token amount and scid
+	public function createTokenTransfer(&$tx,$settings){		
 		//See if use uuid is selected, generate one if so.
 		$unique_identifier = '';
 		if($settings['out_message_uuid'] == 1){
@@ -696,7 +864,7 @@ class Process extends App {
 	
 			
 		//Check for a pending response for this incoming tx
-		$pending_response = $this->processModel->checkForResponseById($tx['order_id']);
+		$pending_response = $this->processModel->checkForPendingResponseByOrderId($tx['order_id']);
 		if($pending_response !==false){
 			//Found a previous repsonse, use that instead of a new one (in case of double response we want the same confirmation number for address submission)
 			$transfer['out_message'] = $pending_response['out_message'];
@@ -713,13 +881,93 @@ class Process extends App {
 		$tx['api_url'] = $settings['api_url'];
 		$tx['out_scid']=$transfer['scid'];
 		$tx['crc32'] = ($unique_identifier == ''?1:crc32($unique_identifier));//not really required for token xfers...
+		$tx['type'] = "token_sale";
+	
+		return (object)["tx"=>$tx,"xfer"=>$transfer_object];
+	}
+
+	//Token refund
+	public function createRefundTokenTransfer(&$tx,$settings){	
+		//No mathcing products / I. Addresses found
+		//Send Refund to buyer
+		
+		$transfer['respond_amount'] = $tx['amount'];
+		$transfer['address'] = $tx['buyer_address'];	
+		$transfer['out_message'] = substr("Refund for: ". $tx['product_label'].'-'.$tx['ia_comment'],0,110);	
+		$transfer['scid'] = "0000000000000000000000000000000000000000000000000000000000000000";
+		$transfer_object=(object)$transfer;	
+		//update unprocessed array
+		$tx['respond_amount'] =  $tx['amount'];
+		$tx['out_message'] = $transfer['out_message'];				
+		$tx['out_message_uuid'] = '';
+		$tx['uuid'] = '';
+		$tx['api_url'] = '';
+		$tx['out_scid']=$transfer['scid'];
+		$tx['crc32'] = '';
+		$tx['type'] = "token_refund";
+
+		return (object)["tx"=>$tx,"xfer"=>$transfer_object];
+	}
+
+
+
+	//Token transfer with token amount and scid
+	public function createSCOTransfer(&$tx,$settings){		
+		//See if use uuid is selected, generate one if so.
+		$unique_identifier = '';
+		if($settings['out_message_uuid'] == 1){
+			$UUID = new UUID;
+			$unique_identifier = $UUID->v4();
+			$settings['out_message'] = $settings['out_message'] . $unique_identifier;
+		}
+					
+		//Use original out message if not a uuid (usually a link or some text)...
+		$transfer['out_message'] = $settings['out_message'];	
+
+		//Use Integrated Address scid if defined.
+		$transfer['scid'] = $settings['scid'];
+		if($settings['ia_scid'] != ''){
+			$transfer['scid'] = $settings['ia_scid'];
+		}
+		//Use scid as out message if message is null.
+		if($transfer['out_message'] == ''){
+			$transfer['out_message'] = $transfer['scid'];
+		}
+		
+		//Use Integrated Address respond amount if defined.
+		$transfer['respond_amount'] = $settings['respond_amount'];//
+		if($settings['ia_respond_amount'] !== '' && $settings['ia_respond_amount'] > 0){
+			$transfer['respond_amount'] = $settings['ia_respond_amount'];
+		}
+
+		$transfer['address'] = $tx['buyer_address'];	
+	
+			
+		//Check for a pending response for this incoming tx
+		$pending_response = $this->processModel->checkForPendingResponseByOrderId($tx['order_id']);
+		if($pending_response !==false){
+			//Found a previous repsonse, use that instead of a new one (in case of double response we want the same confirmation number for address submission)
+			$transfer['out_message'] = $pending_response['out_message'];
+			$unique_identifier = $pending_response['out_message'];
+		}
+			
+		$transfer_object=(object)$transfer;
+		//update unprocessed array
+		//$tx['ia_comment'] = $settings['ia_comment'];
+		$tx['respond_amount'] = $transfer['respond_amount'];
+		$tx['out_message'] = $transfer['out_message'];
+		$tx['out_message_uuid'] = $settings['out_message_uuid'];
+		$tx['uuid'] = $unique_identifier;
+		$tx['api_url'] = $settings['api_url'];
+		$tx['out_scid']=$transfer['scid'];
+		$tx['crc32'] = ($unique_identifier == ''?1:crc32($unique_identifier));//not really required for token/ownership xfers...
 		$tx['type'] = "sc_sale";
 	
 		return (object)["tx"=>$tx,"xfer"=>$transfer_object];
 	}
 
-
-	public function createRefundSCTransfer(&$tx,$settings){	
+	//SC refund
+	public function createRefundSCOTransfer(&$tx,$settings){	
 		//No mathcing products / I. Addresses found
 		//Send Refund to buyer
 		
@@ -740,10 +988,6 @@ class Process extends App {
 
 		return (object)["tx"=>$tx,"xfer"=>$transfer_object];
 	}
-
-
-
-
 
 	
 }
