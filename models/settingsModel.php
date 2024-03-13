22d974a960a51b89e08cdd5e56a224140b28a719
Carl Turechek <carl@siteraiser.com>
2024-03-13 17:17:52 -0400

    Update settingsModel.php

--- models/settingsModel.php
+++ models/settingsModel.php
@@ -14,10 +14,12 @@ class settingsModel extends App {
 	function setSettings(){
 
 		$query="
-		UPDATE settings SET value=:dero_api_ip WHERE name = 'dero_api_ip';			
-		UPDATE settings SET value=:dero_api_port WHERE name = 'dero_api_port';		
-		UPDATE settings SET value=:dero_api_user WHERE name = 'dero_api_user';		
-		UPDATE settings SET value=:dero_api_pass WHERE name = 'dero_api_pass';		
+		UPDATE settings SET value=:daemon_api WHERE name = 'daemon_api';	
+		
+		UPDATE settings SET value=:wallet_api_ip WHERE name = 'wallet_api_ip';			
+		UPDATE settings SET value=:wallet_api_port WHERE name = 'wallet_api_port';		
+		UPDATE settings SET value=:wallet_api_user WHERE name = 'wallet_api_user';		
+		UPDATE settings SET value=:wallet_api_pass WHERE name = 'wallet_api_pass';		
 		UPDATE settings SET value=:web_api_url WHERE name = 'web_api_url';		
 		UPDATE settings SET value=:web_api_user WHERE name = 'web_api_user';		
 		UPDATE settings SET value=:web_api_wallet WHERE name = 'web_api_wallet';	
@@ -25,10 +27,12 @@ class settingsModel extends App {
 		
 		$stmt=$this->pdo->prepare($query);
 		$stmt->execute(array(
-			':dero_api_ip'=>$_POST['dero_api_ip'],
-			':dero_api_port'=>$_POST['dero_api_port'],
-			':dero_api_user'=>$_POST['dero_api_user'],
-			':dero_api_pass'=>$_POST['dero_api_pass'],
+			':daemon_api'=>$_POST['daemon_api'],
+		
+			':wallet_api_ip'=>$_POST['wallet_api_ip'],
+			':wallet_api_port'=>$_POST['wallet_api_port'],
+			':wallet_api_user'=>$_POST['wallet_api_user'],
+			':wallet_api_pass'=>$_POST['wallet_api_pass'],
 			
 			':web_api_url'=>$_POST['web_api_url'],
 			':web_api_user'=>$_POST['web_api_user'],
